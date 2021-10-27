6bf6bf941bc4e52e59ff164cf96ab87d05e0c369
thelinh0302 <chaulinh0302cr7@gmail.com>
2021-10-27 15:52:00 +0700

    update status VN

--- src/app/main/booking/manageBooking/BookingConfig.js
+++ src/app/main/booking/manageBooking/BookingConfig.js
@@ -1,10 +1,10 @@
 export const colorStatus = {
-	Happening: 'rgb(200, 230, 201)',
-	Happened: 'rgb(237 96 111)',
-	'Not yet Happen': '#f1b228'
+	'Đang xảy ra': 'rgb(200, 230, 201)',
+	'Đã xảy ra': 'rgb(237 96 111)',
+	'Chưa xảy ra': '#f1b228'
 };
 export const colorText = {
-	Happening: 'rgb(46, 126, 50)',
-	Happened: 'rgb(251 246 246)',
-	'Not yet Happen': 'rgb(251 246 246)'
+	'Đang xảy ra': 'rgb(46, 126, 50)',
+	'Đã xảy ra': 'rgb(251 246 246)',
+	'Chưa xảy ra': 'rgb(251 246 246)'
 };
